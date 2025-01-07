8ca78e5844ae0fb94cf5ae00dc7f04f0a8323891
Craig Waton <craig@watsonmemorials.co.uk>
2025-01-07 07:57:48 +0000

    Get elements

--- src/main.js
+++ src/main.js
@@ -4,3 +4,8 @@ function getEl(selector) {
   return document.querySelector(selector);
 }
 
+const inputEl = getEl("#unit-input");
+const btnEl = getEl("#btn-convert");
+const lengthEl = getEl("#output-length");
+const volumeEl = getEl("#output-volume");
+const massEl = getEl("#output-mass");
